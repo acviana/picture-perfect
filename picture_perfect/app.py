12a0e1f874c9666f7ff1c1f034aebfa254042624
Alex C. Viana <alexcostaviana@gmail.com>
2020-11-15 13:31:03 -0600

    Seperated calculation and output

--- picture_perfect/app.py
+++ picture_perfect/app.py
@@ -5,13 +5,20 @@ class Picture:
         self.line_of_sight = kwargs["line_of_sight"]
         self.picture_width = kwargs["picture_width"]
         self.picture_height = kwargs["picture_height"]
-        # self.hanger_offset = kwargs["hanger_offset"]
+        # self.hanger_top_offset = kwargs["hanger_top_offset"]
 
     def calc(self):
-        print(f"1/2 Wall Width: {self.wall_width / 2}")
-        print(f"1/2 Picture Width: {self.picture_width / 2}")
-        print(f"1/2 Picture Height: {self.picture_height / 2}")
-        # print()
+        self.half_wall_width = self.wall_width / 2
+        self.half_picture_width = self.picture_width / 2
+        self.half_picture_height = self.picture_height / 2
+
+    def summary(self):
+        print(f"1/2 Wall Width: {self.half_wall_width}")
+        print(f"1/2 Picture Width: {self.half_picture_width}")
+        print(f"1/2 Picture Height: {self.half_picture_height}")
+
+        # print(f"Distance to Bottom of Frame: {self.line_of_sight - }")
+        # print(f"")
 
 
 def main():
@@ -20,9 +27,10 @@ def main():
         line_of_sight=65,
         picture_width=35.25,
         picture_height=46,
-        # hanger_offset=15
+        # hanger_top_offset=15
     )
     picture.calc()
+    picture.summary()
 
 if __name__ == '__main__':
     main()
