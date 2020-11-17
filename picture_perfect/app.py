1401e672f3149af10cd3f6aaaf7f7ad32cd07a15
Alex C. Viana <alexcostaviana@gmail.com>
2020-11-16 21:15:00 -0600

    Added 2-nail mode

--- picture_perfect/app.py
+++ picture_perfect/app.py
@@ -1,10 +1,13 @@
 class Picture:
-    def __init__(self, units="inches", **kwargs):
+    def __init__(self, units="inches", nails=1, **kwargs):
         self.wall_width = kwargs["wall_width"]
         self.line_of_sight = kwargs["line_of_sight"]
         self.picture_width = kwargs["picture_width"]
         self.picture_height = kwargs["picture_height"]
         self.nail_top_offset = kwargs["nail_top_offset"]
+        self.nails = nails
+        if self.nails == 2:
+            self.nail_separation = kwargs["nail_separation"]
 
     def calc(self):
         self.half_wall_width = self.wall_width / 2
@@ -16,7 +19,14 @@ class Picture:
         self.distance_to_frame_top = self.line_of_sight + self.half_picture_height
 
         self.nail_midline_offset = self.half_picture_height - self.nail_top_offset
-        self.nail_final_height = self.line_of_sight + self.nail_midline_offset
+        self.nail_vertical_position = self.line_of_sight + self.nail_midline_offset
+        if self.nails == 1:
+            self.nail_horizontal_position = self.half_wall_width
+        elif self.nails == 2:
+            self.nail_horizontal_position = [
+                self.half_wall_width - (self.nail_separation / 2),
+                self.half_wall_width + (self.nail_separation / 2),
+            ]
 
     def summary(self):
         print(f"1/2 Wall Width: {self.half_wall_width}")
@@ -28,7 +38,7 @@ class Picture:
         print(f"Distance to Frame Top: {self.distance_to_frame_top}")
 
         print(f"Distance from Midline to Nail: {self.nail_midline_offset}")
-        print(f"Final Nail Height: {self.nail_final_height}")
+        print(f"Final Nail Height: {self.nail_vertical_position}")
 
 
 def main():
--- tests/test_picture_perfect.py
+++ tests/test_picture_perfect.py
@@ -6,7 +6,7 @@ def test_version():
     assert __version__ == "0.1.0"
 
 
-def test_app():
+def test_single_nail():
     picture = Picture(
         wall_width=123,
         line_of_sight=71,
@@ -23,4 +23,28 @@ def test_app():
     assert picture.distance_to_frame_midpoint == 71
     assert picture.distance_to_frame_top == 94.0
     assert picture.nail_midline_offset == 8
-    assert picture.nail_final_height == 79
+    assert picture.nail_vertical_position == 79
+    assert picture.nail_horizontal_position == 61.5
+
+
+def test_double_nail():
+    picture = Picture(
+        wall_width=123,
+        line_of_sight=71,
+        picture_width=35.25,
+        picture_height=46,
+        nail_top_offset=15,
+        nails=2,
+        nail_separation=18,
+    )
+    picture.calc()
+
+    assert picture.half_wall_width == 61.5
+    assert picture.half_picture_width == 17.625
+    assert picture.half_picture_height == 23.0
+    assert picture.distance_to_frame_bottom == 48.0
+    assert picture.distance_to_frame_midpoint == 71
+    assert picture.distance_to_frame_top == 94.0
+    assert picture.nail_midline_offset == 8
+    assert picture.nail_vertical_position == 79
+    assert picture.nail_horizontal_position == [52.5, 70.5]
